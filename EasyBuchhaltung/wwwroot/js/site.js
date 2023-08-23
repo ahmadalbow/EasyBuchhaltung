69fc7f60fe71a17037e68953275996f7608d4839
ahmad <ahmad@DESKTOP-7KMFBKN>
2023-08-23 13:24:01 +0200

    last update

--- EasyBuchhaltung/wwwroot/js/site.js
+++ EasyBuchhaltung/wwwroot/js/site.js
@@ -1,4 +1,22 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿const planTypeSelecter = document.querySelector(".plan-type-selecter");
+const jearlich = document.querySelector(".jearlich");
+const montalich = document.querySelector(".monatlich");
+const mover = document.querySelector(".mover")
+planTypeSelecter.setAttribute("plan", "jearlich");
 
-// Write your JavaScript code.
+planTypeSelecter.addEventListener("click", () => {
+    if (planTypeSelecter.getAttribute("plan") == "jearlich") {
+        planTypeSelecter.setAttribute("plan", "monatlich")
+        mover.classList.add("left-to-right");
+        mover.classList.remove("right-to-left");
+        jearlich.classList.add("text-black-50");
+        montalich.classList.remove("text-black-50");
+    }
+    else if (planTypeSelecter.getAttribute("plan") == "monatlich") {
+        planTypeSelecter.setAttribute("plan", "jearlich")
+        mover.classList.remove("left-to-right");
+        mover.classList.add("right-to-left");
+        montalich.classList.add("text-black-50");
+        jearlich.classList.remove("text-black-50");
+    }
+})
